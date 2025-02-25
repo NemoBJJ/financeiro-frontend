01723eccdcf342f47217198c50e92280e7c01101
NemoBJJ <engnfaraujo@gmail.com>
2025-02-25 22:41:45 +0000

    Financeiro FrontEnd HTTPS

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// Configuração para sempre utilizar o IP Elástico
+// Configuração para utilizar o IP elástico com HTTP
 const api = axios.create({
-    baseURL: 'http://3.217.55.187:8084/api', // IP elástico
+    baseURL: 'https://api2.neemindev.com', // Usando o IP elástico com HTTP e porta 8085
 });
 
 export default api;
